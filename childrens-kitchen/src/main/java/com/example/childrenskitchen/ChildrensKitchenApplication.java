29bc81d56c593d866b539d3ad2b780902d91d049
Tanya Rashkova <86685075+tanijant@users.noreply.github.com>
2023-05-02 20:32:29 +0300

    Initial commit

---
+++ childrens-kitchen/src/main/java/com/example/childrenskitchen/ChildrensKitchenApplication.java
@@ -0,0 +1,13 @@
+package com.example.childrenskitchen;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class ChildrensKitchenApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(ChildrensKitchenApplication.class, args);
+    }
+
+}
---
+++ childrens-kitchen/src/test/java/com/example/childrenskitchen/ChildrensKitchenApplicationTests.java
@@ -0,0 +1,13 @@
+package com.example.childrenskitchen;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class ChildrensKitchenApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
